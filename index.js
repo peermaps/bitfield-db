629ecf67818cddaf08d0d691e80b8a724dc08920
substack <substack@bits.coop>
2019-07-14 17:22:00 -1000

    array -> array updates work

--- index.js
+++ index.js
@@ -74,33 +74,94 @@ Roaring.prototype.select = function (i) {
 }
 
 Roaring.prototype.flush = function (cb) {
+  var self = this
+  var errored = false
   var ikeys = Object.keys(this._inserts)
+  var pending = 1
   for (var i = 0; i < ikeys.length; i++) {
     var set = this._inserts[ikeys[i]]
     // todo: run container
-    if (set.length < 4096) { // array
-      set.sort(cmp)
-      var buf = Buffer.alloc(1 + set.length*2)
-      buf[0] = ARRAY
-      for (var j = 0; j < set.length; j++) {
-        buf.writeUInt16BE(set[j],1+j*2)
-      }
-      this._db.put(String(ikeys[i]), buf)
-    } else { // bitfield
-      var buf = Buffer.alloc(8193)
-      buf[0] = BITFIELD
-      for (var j = 0; j < set.length; j++) {
-        var x = set[j]
-        var xi = 1+Math.floor(x/8)
-        buf[xi] = buf[xi] | (1<<(x%8))
-      }
-      this._db.put(String(ikeys[i]), buf)
-    }
+    pending++
+    this._merge(String(ikeys[i]), set, done)
   }
-  this._db.flush(cb)
+  done()
+  function done (err) {
+    if (errored) return
+    if (err) {
+      errored = true
+      return cb(err)
+    }
+    if (--pending !== 0) return
+    self._inserts = {}
+    self._deletes = {}
+    self._db.flush(cb)
+  }
+}
+
+Roaring.prototype._merge = function (key, set, cb) {
+  var self = this
+  self._db.get(key, function (err, node) {
+    if (err) {
+      cb(err)
+    } else if (!node && set.length < 4096) { // array
+      set.sort(cmp)
+      self._db.put(key, buildSet(set))
+      cb()
+    } else if (!node) { // bitfield
+      self._db.put(key, buildBitfield(set))
+      cb()
+    } else if (node.value[0] === ARRAY
+    && (node.value.length-1)/2 + set.length < 4096) { // array -> array
+      console.log('!!! array -> array')
+      expandSetWithArrayData(set, node.value)
+      set.sort(cmp)
+      self._db.put(key, buildSet(set))
+      cb()
+    } else if (node.value[0] === ARRAY) { // array -> bitfield
+      console.log('!!! array -> bitfield')
+      expandSetWithArrayData(set, node.value)
+      self._db.put(key, buildBitfield(set))
+      cb()
+    } else if (node.value[0] === BITFIELD) { // bitfield -> bitfield
+      console.log('!!! bitfield -> bitfield')
+      writeIntoBitfieldData(set, node.value)
+      self._db.put(key, node.value)
+      cb()
+    }
+  })
 }
 
 function cmp (a, b) { return a < b ? -1 : +1 }
+
+function buildSet (set) {
+  var buf = Buffer.alloc(1 + set.length*2)
+  buf[0] = ARRAY
+  for (var j = 0; j < set.length; j++) {
+    buf.writeUInt16BE(set[j],1+j*2)
+  }
+  return buf
+}
+
+function buildBitfield (set) {
+  var buf = Buffer.alloc(8193)
+  buf[0] = BITFIELD
+  writeIntoBitfieldData(set, buf)
+  return buf
+}
+
+function expandSetWithArrayData (set, buf) {
+  for (var i = 1; i < buf.length; i+=2) {
+    set.push(buf.readUInt16BE(i))
+  }
+}
+
+function writeIntoBitfieldData (set, buf) {
+  for (var i = 0; i < set.length; i++) {
+    var x = set[i]
+    var xi = 1+Math.floor(x/8)
+    buf[xi] = buf[xi] | (1<<(x%8))
+  }
+}
 
 /*
 function msb (x) { return 31 - Math.clz32(x) }
---
+++ test/update.js
@@ -0,0 +1,39 @@
+const RSet = require('../')
+const ram = require('random-access-memory')
+const TinyBox = require('tinybox')
+const test = require('tape')
+
+test('array update', function (t) {
+  t.plan(402)
+  var rset = new RSet(new TinyBox(ram()))
+  var set = new Set([5,10,15,20,23,24,26,27])
+  set.forEach(x => rset.add(x))
+  rset.flush(function (err) {
+    t.ifError(err)
+    var pending = 51
+    for (var i = 0; i < 50; i++) (function (i) {
+      rset.has(i, function (err, ex) {
+        t.ifError(err)
+        t.equal(ex, set.has(i), 'has '+i+': '+set.has(i))
+        if (--pending === 0) update()
+      })
+    })(i)
+    if (--pending === 0) update()
+  })
+  function update () {
+    var add = [0,9,21,28,100,101,102,120,146,147,149]
+    add.forEach(x => {
+      rset.add(x)
+      set.add(x)
+    })
+    rset.flush(function (err) {
+      t.ifError(err)
+      for (var i = 0; i < 150; i++) (function (i) {
+        rset.has(i, function (err, ex) {
+          t.ifError(err)
+          t.equal(ex, set.has(i), 'after update has '+i+': '+set.has(i))
+        })
+      })(i)
+    })
+  }
+})
