71394ab5edf9f92cf9d00eeaef017ccc4c51a037
substack <substack@bits.coop>
2019-07-24 11:32:48 -1000

    run rank test with updates

--- test/rank.js
+++ test/rank.js
@@ -154,3 +154,158 @@ test('multi bitfield rank', function (t) {
     })
   })
 })
+
+test('single run rank', function (t) {
+  t.plan(2*200+1)
+  var rset = new RSet(ram())
+  var set = new Set
+  var expected = {}
+  var sum = 0
+  for (var i = 0; i < 50; i++) {
+    expected[i] = sum
+  }
+  for (var i = 50; i < 150; i++) {
+    set.add(i)
+    rset.add(i)
+    expected[i] = sum++
+  }
+  for (var i = 150; i < 200; i++) {
+    expected[i] = sum
+  }
+  rset.flush(function (err) {
+    t.ifError(err)
+    for (var i = 0; i < 200; i++) (function (i) {
+      rset.rank(i, function (err, res) {
+        t.ifError(err)
+        t.equal(res, expected[i], `rank(${i})`)
+      })
+    })(i)
+  })
+})
+
+test('several runs rank', function (t) {
+  t.plan(2*200+1)
+  var rset = new RSet(ram())
+  var set = new Set
+  var expected = {}
+  var sum = 0
+  for (var i = 0; i < 10; i++) {
+    expected[i] = sum
+  }
+  for (var i = 10; i < 50; i++) {
+    set.add(i)
+    rset.add(i)
+    expected[i] = sum++
+  }
+  for (var i = 50; i < 80; i++) {
+    expected[i] = sum
+  }
+  for (var i = 80; i < 120; i++) {
+    set.add(i)
+    rset.add(i)
+    expected[i] = sum++
+  }
+  for (var i = 120; i < 125; i++) {
+    expected[i] = sum
+  }
+  for (var i = 125; i < 150; i++) {
+    set.add(i)
+    rset.add(i)
+    expected[i] = sum++
+  }
+  for (var i = 150; i < 155; i++) {
+    expected[i] = sum
+  }
+  for (var i = 155; i < 170; i++) {
+    set.add(i)
+    rset.add(i)
+    expected[i] = sum++
+  }
+  for (var i = 170; i < 200; i++) {
+    expected[i] = sum
+  }
+  rset.flush(function (err) {
+    t.ifError(err)
+    for (var i = 0; i < 200; i++) (function (i) {
+      rset.rank(i, function (err, res) {
+        t.ifError(err)
+        t.equal(res, expected[i])
+      })
+    })(i)
+  })
+})
+
+test('several runs with updates', function (t) {
+  t.plan(2*200*2+2)
+  var rset = new RSet(ram())
+  var set = new Set
+  var expected = {}
+  var sum = 0
+  for (var i = 0; i < 10; i++) {
+    expected[i] = sum
+  }
+  for (var i = 10; i < 50; i++) {
+    set.add(i)
+    rset.add(i)
+    expected[i] = sum++
+  }
+  for (var i = 50; i < 80; i++) {
+    expected[i] = sum
+  }
+  for (var i = 80; i < 120; i++) {
+    set.add(i)
+    rset.add(i)
+    expected[i] = sum++
+  }
+  for (var i = 120; i < 125; i++) {
+    expected[i] = sum
+  }
+  for (var i = 125; i < 150; i++) {
+    set.add(i)
+    rset.add(i)
+    expected[i] = sum++
+  }
+  for (var i = 150; i < 155; i++) {
+    expected[i] = sum
+  }
+  for (var i = 155; i < 170; i++) {
+    set.add(i)
+    rset.add(i)
+    expected[i] = sum++
+  }
+  for (var i = 170; i < 200; i++) {
+    expected[i] = sum
+  }
+  rset.flush(function (err) {
+    t.ifError(err)
+    var pending = 201
+    for (var i = 0; i < 200; i++) (function (i) {
+      rset.rank(i, function (err, res) {
+        t.ifError(err)
+        t.equal(res, expected[i], `rank(${i})`)
+        if (--pending === 0) update()
+      })
+    })(i)
+    if (--pending === 0) update()
+  })
+  function update () {
+    var sum = expected[120]
+    for (var i = 120; i < 125; i++) {
+      set.add(i)
+      rset.add(i)
+      expected[i] = sum++
+    }
+    for (var i = 125; i < 200; i++) {
+      expected[i] += 5
+    }
+    rset.flush(function (err) {
+      t.ifError(err)
+      for (var i = 0; i < 200; i++) (function (i) {
+        rset.rank(i, function (err, res) {
+          t.ifError(err)
+          t.equal(res, expected[i], `rank(${i})`)
+        })
+      })(i)
+    })
+  }
+})
